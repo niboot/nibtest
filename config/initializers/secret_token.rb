4c8e6ab54106a9f4988f2560ad51912dcfe2d4c7
Nicolas Bayet <niboot@gmail.com>
2011-03-11 18:49:27 +0100

    Premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nibtest::Application.config.secret_token = '2732afc8b297504d8146130eef731c80141566856b3fe55316a882e32b8cf0ea061de6405ab94ca6eb230b758a445dc4a77b4361a4a10d6090c1cb61d22d0802'
